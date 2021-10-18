b86466f72fcd57c2ec4dc55b65db957443a00e37
rohitsainier <rohitsainier@gmail.com>
2021-10-18 23:26:45 +0530

    Core Data setup

---
+++ Tasker/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Tasker
+//
+//  Created by Rohit Saini on 18/10/21.
+//
+
+import Foundation
+
+// MARK: - FORMATTER
+ let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
+
+
