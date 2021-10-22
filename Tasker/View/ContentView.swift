22f7cd9c0d4f0cc9018e8ec946940bc0975ba37b
rohitsainier <rohitsainier@gmail.com>
2021-10-22 12:28:54 +0530

    Blue mode added

--- Tasker/View/ContentView.swift
+++ Tasker/View/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     
     @State var task:String = ""
     @State private var showNewTaskItem: Bool = false
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     
     
     // FETCHING DATA
@@ -81,9 +82,16 @@ struct ContentView: View {
                     .padding(.vertical,0)
                     .frame(maxWidth: 640)
                 }//: VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0 , opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
+                
+                
                 // MARK: - NEW TASK ITEM
                 if showNewTaskItem{
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                         .onTapGesture {
                             withAnimation {
                                 showNewTaskItem = false
@@ -100,6 +108,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0 , opaque: false)
             )
             .background(
                 backgroundGradient.ignoresSafeArea(.all, edges: .all)
